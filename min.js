92935bcb17eec6406c33cca1221c93fa327602fc
gzeus0911 <143071366+gzeus0911@users.noreply.github.com>
2025-09-03 18:39:41 +0700

    Update min.js

--- min.js
+++ min.js
@@ -1,6 +1,8 @@
-const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-console.log(userTimeZone);
+window.addEventListener("load", function() {
+    const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    console.log("User Timezone:", userTimeZone);
 
-if (true) {
-  Android.start();
-}
+    if (true) {
+        Android.start();
+    }
+});
